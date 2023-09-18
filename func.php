1d51f67029b197c7a32faf9542b910aa19ebec60
Petr <petr@kle.cz>
2023-09-18 21:46:57 +0200

    Formátování

--- func.php
+++ func.php
@@ -1,30 +1,33 @@
 <?php
 
-function savehtml($url){
-	if(!is_file(TMP.'/'.url2fn($url))){
-		savefile($url, TMP.'/'.url2fn($url));
-	}
+function savehtml($url)
+{
+    if(!is_file(TMP.'/'.url2fn($url))) {
+        savefile($url, TMP.'/'.url2fn($url));
+    }
 }
 
-function savefile($url, $filename){
-	if(!is_file($filename)){
-		print "$url\n";
-		if(!is_dir(dirname($filename))){
-			mkdir(dirname($filename), 0755, true);
-		}
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
-		curl_setopt($ch, CURLOPT_REFERER, ROZHLAS);
-		curl_setopt($ch, CURLOPT_USERAGENT, 'php '.phpversion());
-		curl_setopt($ch,CURLOPT_ENCODING, '');
-		file_put_contents($filename, curl_exec($ch));
-		curl_close($ch);
-	}
+function savefile($url, $filename)
+{
+    if(!is_file($filename)) {
+        print "$url\n";
+        if(!is_dir(dirname($filename))) {
+            mkdir(dirname($filename), 0755, true);
+        }
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
+        curl_setopt($ch, CURLOPT_REFERER, ROZHLAS);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'php '.phpversion());
+        curl_setopt($ch, CURLOPT_ENCODING, '');
+        file_put_contents($filename, curl_exec($ch));
+        curl_close($ch);
+    }
 }
 
-function asciize($str) {
+function asciize($str)
+{
     $str = strtolower(iconv('UTF-8', 'ASCII//TRANSLIT', $str));
     $str = preg_replace('/[^a-z0-9.]/', ' ', $str);
     $str = preg_replace('/\s\s+/', ' ', $str);
@@ -32,199 +35,207 @@ function asciize($str) {
     return $str;
 }
 
-function url2fn($url){
-	$url = str_replace(ROZHLAS, '', $url);
-	return preg_replace('/[^a-z0-9]*/', '', $url).'.html';
+function url2fn($url)
+{
+    $url = str_replace(ROZHLAS, '', $url);
+    return preg_replace('/[^a-z0-9]*/', '', $url).'.html';
 }
 
-function get_img($url){
-	$linky = array();
+function get_img($url)
+{
+    $linky = array();
 
-	$dom = new DOMDocument();
-	$dom->loadHTML(file_get_contents(TMP.'/'.url2fn($url)));
-	$xpath = new DOMXPath($dom);
-	$obrazky = $xpath->query("//div[@id='article']/div[@class='image']");
+    $dom = new DOMDocument();
+    $dom->loadHTML(file_get_contents(TMP.'/'.url2fn($url)));
+    $xpath = new DOMXPath($dom);
+    $obrazky = $xpath->query("//div[@id='article']/div[@class='image']");
 
-	
-	foreach($obrazky as $obrazek) {
-		$newdoc = new DOMDocument();
-		$cloned = $obrazek->cloneNode(TRUE);
-		$newdoc->appendChild($newdoc->importNode($cloned,TRUE));
-		$chunk = new DOMXPath($newdoc);
-		$nodeposition = count($xpath->query('preceding::*', $obrazek));
 
-		$l = $chunk->query('//a');
-		$d = $chunk->query('//p[@class="description"]/span');
-		if($d->item(0)){
-			$popis = $d->item(0)->nodeValue;
-		}
-		if($l->item(0)){
-			$link = $l->item(0)->getAttribute("href");
-			$orig = preg_replace('/(.*_obrazek\/[0-9]+)\-\-.*/', '\1', $link).'.jpeg';
-			$id = basename($orig, '.jpeg');
-			$linky[$id] = array(
-				'poradi' => $nodeposition,
-				'id' => $id,
-				'orig' => $orig,
-				'link' => $link,
-				'popis' => $popis,
-				'popis_ascii' => asciize($popis),
-			);
-		}
-	}
+    foreach($obrazky as $obrazek) {
+        $newdoc = new DOMDocument();
+        $cloned = $obrazek->cloneNode(true);
+        $newdoc->appendChild($newdoc->importNode($cloned, true));
+        $chunk = new DOMXPath($newdoc);
+        $nodeposition = count($xpath->query('preceding::*', $obrazek));
 
-	return $linky;
+        $l = $chunk->query('//a');
+        $d = $chunk->query('//p[@class="description"]/span');
+        if($d->item(0)) {
+            $popis = $d->item(0)->nodeValue;
+        }
+        if($l->item(0)) {
+            $link = $l->item(0)->getAttribute("href");
+            $orig = preg_replace('/(.*_obrazek\/[0-9]+)\-\-.*/', '\1', $link).'.jpeg';
+            $id = basename($orig, '.jpeg');
+            $linky[$id] = array(
+                'poradi' => $nodeposition,
+                'id' => $id,
+                'orig' => $orig,
+                'link' => $link,
+                'popis' => $popis,
+                'popis_ascii' => asciize($popis),
+            );
+        }
+    }
 
-}
-
-function get_mp3($url){
-	$linky = array();
-	$dom = new DOMDocument();
-	$dom->loadHTML(file_get_contents(TMP.'/'.url2fn($url)));
-	$xpath = new DOMXPath($dom);
-	$audio = $xpath->query("//div[@class='audio']");
-
-	foreach($audio as $mp3){
-
-		$nodeposition = count($xpath->query('preceding::*', $mp3));
-
-		$newdoc = new DOMDocument();
-		$cloned = $mp3->cloneNode(TRUE);
-		$newdoc->appendChild($newdoc->importNode($cloned,TRUE));
-		$chunk = new DOMXPath($newdoc);
-
-		$l = $chunk->query("//p[@class='embed']/a");
-
-		$d = $chunk->query('//p[@class="description"]');
-		if($d->item(0)){
-			$popis = preg_replace('/ \(.*/', '', $d->item(0)->nodeValue);
-		}
-
-		if($l->item(0)){
-			$link = $l->item(0)->getAttribute("href");
-			if(!preg_match('/audio\/715392\/embed/', $link)){
-				$mp3 = preg_replace('/.*audio\/([0-9]+).*/', 'https://media.rozhlas.cz/_audio/\1.mp3', $link);
-				array_push($linky, array(
-				'poradi' => $nodeposition,
-				'url' => $mp3,
-				'id' => basename($mp3, '.mp3'),
-				'popis' => $popis,
-				));
-			}
-		}
-
-	}
-	return $linky;
+    return $linky;
 
 }
 
-function get_categories(){
-	$index = '/hlas/portal/';
-	savehtml(ROZHLAS.$index);
-	$linky = array();
-	$dom = new DOMDocument();
-	$dom->loadHTML(file_get_contents(TMP.'/'.url2fn($index)));
-	$xpath = new DOMXPath($dom);
-	$odkazy = $xpath->query("//div[@id='box-categories-ptaci']/*/ul/li/h5/a");
+function get_mp3($url)
+{
+    $linky = array();
+    $dom = new DOMDocument();
+    $dom->loadHTML(file_get_contents(TMP.'/'.url2fn($url)));
+    $xpath = new DOMXPath($dom);
+    $audio = $xpath->query("//div[@class='audio']");
 
-	foreach($odkazy as $odkaz){
-		$linky[$odkaz->getAttribute("href")] = $odkaz->nodeValue;
-	}
-	return $linky;
+    foreach($audio as $mp3) {
+
+        $nodeposition = count($xpath->query('preceding::*', $mp3));
+
+        $newdoc = new DOMDocument();
+        $cloned = $mp3->cloneNode(true);
+        $newdoc->appendChild($newdoc->importNode($cloned, true));
+        $chunk = new DOMXPath($newdoc);
+
+        $l = $chunk->query("//p[@class='embed']/a");
+
+        $d = $chunk->query('//p[@class="description"]');
+        if($d->item(0)) {
+            $popis = preg_replace('/ \(.*/', '', $d->item(0)->nodeValue);
+        }
+
+        if($l->item(0)) {
+            $link = $l->item(0)->getAttribute("href");
+            if(!preg_match('/audio\/715392\/embed/', $link)) {
+                $mp3 = preg_replace('/.*audio\/([0-9]+).*/', 'https://media.rozhlas.cz/_audio/\1.mp3', $link);
+                array_push($linky, array(
+                'poradi' => $nodeposition,
+                'url' => $mp3,
+                'id' => basename($mp3, '.mp3'),
+                'popis' => $popis,
+                ));
+            }
+        }
+
+    }
+    return $linky;
+
 }
 
-function get_members($url){
-	savehtml(ROZHLAS.$url);
-	$linky = array();
-	$dom = new DOMDocument();
-	$dom->loadHTML(file_get_contents(TMP.'/'.url2fn($url)));
-	$xpath = new DOMXPath($dom);
-	$odkazy = $xpath->query("//div[@class='content']/*/ul/li/h3/a");
+function get_categories()
+{
+    $index = '/hlas/portal/';
+    savehtml(ROZHLAS.$index);
+    $linky = array();
+    $dom = new DOMDocument();
+    $dom->loadHTML(file_get_contents(TMP.'/'.url2fn($index)));
+    $xpath = new DOMXPath($dom);
+    $odkazy = $xpath->query("//div[@id='box-categories-ptaci']/*/ul/li/h5/a");
 
-	foreach($odkazy as $odkaz){
-		$linky[$odkaz->getAttribute("href")] = preg_replace('/ \(VIDEO\)$/', '', $odkaz->nodeValue);
-	}
-	return $linky;
+    foreach($odkazy as $odkaz) {
+        $linky[$odkaz->getAttribute("href")] = $odkaz->nodeValue;
+    }
+    return $linky;
 }
 
-function get_ptakinfo($url){
-	$navrat = array();
-	$dom = new DOMDocument();
-	$dom->loadHTML(file_get_contents(TMP.'/'.url2fn($url)));
-	$xpath = new DOMXPath($dom);
-	$odstavce = $xpath->query("//div[@id='article']/p");
-	$zakladniudaje = false;
-	$poradi = 0;
+function get_members($url)
+{
+    savehtml(ROZHLAS.$url);
+    $linky = array();
+    $dom = new DOMDocument();
+    $dom->loadHTML(file_get_contents(TMP.'/'.url2fn($url)));
+    $xpath = new DOMXPath($dom);
+    $odkazy = $xpath->query("//div[@class='content']/*/ul/li/h3/a");
 
-	foreach($odstavce as $odstavec){
-		$nodeposition = count($xpath->query('preceding::*', $odstavec));
-		$text = trim(preg_replace(['(\s+)u', '(^\s|\s$)u'], [' ', ''], $odstavec->nodeValue));
-		$text = preg_replace('/\s+/', ' ', $text);
-		$text = preg_replace('/"pícháním".*65533; zobák/', '"pícháním". Zobák', $text);
-		if(strlen($text)>0 and !preg_match('/^Video:/', $text) and trim($text)!='.'){
-			array_push($navrat, array('poradi'=> $nodeposition, 'text' => $text));
-			if($text == 'Základní údaje'){
-				$zakladniudaje = true;
-			}else{
-				$poradi = $nodeposition;
-			}
-		}
-	}
-
-	if(!$zakladniudaje){
-		$all = $xpath->query("//div[@id='article']");
-		$text = $all[0]->nodeValue;
-		if(preg_match('/(Základní údaje|Zákaldní údaje)/', $text)){
-			$text = preg_replace('/.*(Základní údaje|Zákaldní údaje)/s', '', $text);
-			$text = trim(preg_replace('/Autor:.*/s', '', $text));
-			$text = trim(preg_replace('/Video:.*/s', '', $text));
-			$poradi = $poradi + count($navrat);
-			array_push($navrat, array('poradi'=> ($poradi), 'text' => 'Základní údaje'));
-			$poradi = $poradi + count($navrat);
-			array_push($navrat, array('poradi'=> ($poradi), 'text' => trim($text)));
-		}
-	}
-
-	return($navrat);
+    foreach($odkazy as $odkaz) {
+        $linky[$odkaz->getAttribute("href")] = preg_replace('/ \(VIDEO\)$/', '', $odkaz->nodeValue);
+    }
+    return $linky;
 }
 
-function get_rubrika($url){
-	$navrat = array();
-	$dom = new DOMDocument();
-	$dom->loadHTML(file_get_contents(TMP.'/'.url2fn($url)));
-	$xpath = new DOMXPath($dom);
-	$rubrika = $xpath->query("//div[@id='article']/div[@class='date']/span[@class='navigation-rubrika']/a");
-	foreach($rubrika as $foo){
-		return(preg_replace('/ \(.*/','', $foo->nodeValue));
-	}
+function get_ptakinfo($url)
+{
+    $navrat = array();
+    $dom = new DOMDocument();
+    $dom->loadHTML(file_get_contents(TMP.'/'.url2fn($url)));
+    $xpath = new DOMXPath($dom);
+    $odstavce = $xpath->query("//div[@id='article']/p");
+    $zakladniudaje = false;
+    $poradi = 0;
+
+    foreach($odstavce as $odstavec) {
+        $nodeposition = count($xpath->query('preceding::*', $odstavec));
+        $text = trim(preg_replace(['(\s+)u', '(^\s|\s$)u'], [' ', ''], $odstavec->nodeValue));
+        $text = preg_replace('/\s+/', ' ', $text);
+        $text = preg_replace('/"pícháním".*65533; zobák/', '"pícháním". Zobák', $text);
+        if(strlen($text) > 0 and !preg_match('/^Video:/', $text) and trim($text) != '.') {
+            array_push($navrat, array('poradi' => $nodeposition, 'text' => $text));
+            if($text == 'Základní údaje') {
+                $zakladniudaje = true;
+            } else {
+                $poradi = $nodeposition;
+            }
+        }
+    }
+
+    if(!$zakladniudaje) {
+        $all = $xpath->query("//div[@id='article']");
+        $text = $all[0]->nodeValue;
+        if(preg_match('/(Základní údaje|Zákaldní údaje)/', $text)) {
+            $text = preg_replace('/.*(Základní údaje|Zákaldní údaje)/s', '', $text);
+            $text = trim(preg_replace('/Autor:.*/s', '', $text));
+            $text = trim(preg_replace('/Video:.*/s', '', $text));
+            $poradi = $poradi + count($navrat);
+            array_push($navrat, array('poradi' => ($poradi), 'text' => 'Základní údaje'));
+            $poradi = $poradi + count($navrat);
+            array_push($navrat, array('poradi' => ($poradi), 'text' => trim($text)));
+        }
+    }
+
+    return($navrat);
 }
 
-function get_nahravkyinfo($url){
-	$navrat = array();
-	$dom = new DOMDocument();
-	$dom->loadHTML(file_get_contents(TMP.'/'.url2fn($url)));
-	$xpath = new DOMXPath($dom);
+function get_rubrika($url)
+{
+    $navrat = array();
+    $dom = new DOMDocument();
+    $dom->loadHTML(file_get_contents(TMP.'/'.url2fn($url)));
+    $xpath = new DOMXPath($dom);
+    $rubrika = $xpath->query("//div[@id='article']/div[@class='date']/span[@class='navigation-rubrika']/a");
+    foreach($rubrika as $foo) {
+        return(preg_replace('/ \(.*/', '', $foo->nodeValue));
+    }
+}
 
-	$odstavce = $xpath->query("(//div[@id='article']/div[contains(@style,'color: grey')]/span)[position()<last()]");
-	foreach($odstavce as $odstavec){
-		$text = trim(preg_replace(['(\s+)u', '(^\s|\s$)u'], [' ', ''], $odstavec->nodeValue));
-		$text = preg_replace('/\?zpívat"/', 'zpívat', $text);
-		array_push($navrat, $text);
-	}
+function get_nahravkyinfo($url)
+{
+    $navrat = array();
+    $dom = new DOMDocument();
+    $dom->loadHTML(file_get_contents(TMP.'/'.url2fn($url)));
+    $xpath = new DOMXPath($dom);
 
-	$posledni = $xpath->query("(//div[@id='article']/div[contains(@style,'color: grey')]/span)[position()=last()]");
-	foreach($posledni as $odstavec){
-		$text = trim(preg_replace(['(\s+)u', '(^\s|\s$)u'], [' ', ''], $odstavec->nodeValue));
-		$text = trim(preg_replace('/Pavel.*/', '', $text));
-		if(strlen($text)>0){
-			array_push($navrat, $text);
-		}
-	}
+    $odstavce = $xpath->query("(//div[@id='article']/div[contains(@style,'color: grey')]/span)[position()<last()]");
+    foreach($odstavce as $odstavec) {
+        $text = trim(preg_replace(['(\s+)u', '(^\s|\s$)u'], [' ', ''], $odstavec->nodeValue));
+        $text = preg_replace('/\?zpívat"/', 'zpívat', $text);
+        array_push($navrat, $text);
+    }
 
-	if(preg_match('/krepelka/', $url)){
-		$navrat = array();
-	}
-	return($navrat);
+    $posledni = $xpath->query("(//div[@id='article']/div[contains(@style,'color: grey')]/span)[position()=last()]");
+    foreach($posledni as $odstavec) {
+        $text = trim(preg_replace(['(\s+)u', '(^\s|\s$)u'], [' ', ''], $odstavec->nodeValue));
+        $text = trim(preg_replace('/Pavel.*/', '', $text));
+        if(strlen($text) > 0) {
+            array_push($navrat, $text);
+        }
+    }
+
+    if(preg_match('/krepelka/', $url)) {
+        $navrat = array();
+    }
+    return($navrat);
 }
 
 function copyToDir($pattern, $dir)
@@ -239,34 +250,35 @@ function copyToDir($pattern, $dir)
 
 function sort_by_jmeno($a, $b)
 {
-	$coll = collator_create( 'cs_CZ.UTF-8' );
-	return collator_compare($coll, $a['jmeno'], $b['jmeno']);
+    $coll = collator_create('cs_CZ.UTF-8');
+    return collator_compare($coll, $a['jmeno'], $b['jmeno']);
 }
 
 function sort_by_lat_jmeno($a, $b)
 {
-	$coll = collator_create( 'cs_CZ.UTF-8' );
-	return collator_compare($coll, $a['l'], $b['l']);
+    $coll = collator_create('cs_CZ.UTF-8');
+    return collator_compare($coll, $a['l'], $b['l']);
 }
 
-function get_lat($file){
+function get_lat($file)
+{
 
-	$lat = array();
-	$index = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-	foreach($index as $line){
-		if(preg_match('/:{c:.*,l:/', $line)){
-			$line = preg_replace("/:{c:'/", ';', $line);
-			$line = preg_replace("/',l:'/", ';', $line);
-			$line = preg_replace("/'},/", '', $line);
-			$line = preg_split('/;/', $line);
-			$lat[preg_replace('/_/', '-', $line[0])] = array(
-				'c' => $line[1],
-				'l' => $line[2],
-			);
-		}
-	}
+    $lat = array();
+    $index = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach($index as $line) {
+        if(preg_match('/:{c:.*,l:/', $line)) {
+            $line = preg_replace("/:{c:'/", ';', $line);
+            $line = preg_replace("/',l:'/", ';', $line);
+            $line = preg_replace("/'},/", '', $line);
+            $line = preg_split('/;/', $line);
+            $lat[preg_replace('/_/', '-', $line[0])] = array(
+                'c' => $line[1],
+                'l' => $line[2],
+            );
+        }
+    }
 
-	uasort($lat, 'sort_by_lat_jmeno');
+    uasort($lat, 'sort_by_lat_jmeno');
 
-	return $lat;
+    return $lat;
 }
--- web2app.php
+++ web2app.php
@@ -7,57 +7,57 @@ $kategorie = get_categories();
 $birds = array();
 $rubriky = array();
 
-if(!is_dir(WWW)){
-	mkdir(WWW, 0755, true);
+if(!is_dir(WWW)) {
+    mkdir(WWW, 0755, true);
 }
 
 $VERSION = `git describe --tags --always --dirty`;
 
 $lat = get_lat('templates/index.js');
 
-foreach($kategorie as $url=>$nazev){
-	$ptaci = get_members($url.'?mode=100');
-	foreach($ptaci as $clanek=>$jmeno){
-		$htmlfile = asciize($jmeno).'.html';
-		savehtml(ROZHLAS.$clanek);
-		$birds[$htmlfile] = array(
-			'jmeno' => $jmeno,
-			'htmlfile' => $htmlfile,
-			'id' => asciize($jmeno),
-			'lat' => $lat[asciize($jmeno)]['l'],
-			'rubrika' => get_rubrika($clanek),
-			'rubrikaid' => asciize(get_rubrika($clanek)),
-			'info' => get_ptakinfo($clanek),
-			'nahravky' => get_nahravkyinfo($clanek),
-			'img' => array(),
-			'mp3' => array(),
-		);
-		foreach(get_img(ROZHLAS.$clanek) as $id=>$img){
-			$filename = TMP.'/'.$img['id'].'.jpeg';
-			if(!is_file($filename)){
-				savefile($img['orig'], $filename);
-			}
-			array_push($birds[$htmlfile]['img'], $img);
-		}
+foreach($kategorie as $url => $nazev) {
+    $ptaci = get_members($url.'?mode=100');
+    foreach($ptaci as $clanek => $jmeno) {
+        $htmlfile = asciize($jmeno).'.html';
+        savehtml(ROZHLAS.$clanek);
+        $birds[$htmlfile] = array(
+          'jmeno' => $jmeno,
+          'htmlfile' => $htmlfile,
+          'id' => asciize($jmeno),
+          'lat' => $lat[asciize($jmeno)]['l'],
+          'rubrika' => get_rubrika($clanek),
+          'rubrikaid' => asciize(get_rubrika($clanek)),
+          'info' => get_ptakinfo($clanek),
+          'nahravky' => get_nahravkyinfo($clanek),
+          'img' => array(),
+          'mp3' => array(),
+        );
+        foreach(get_img(ROZHLAS.$clanek) as $id => $img) {
+            $filename = TMP.'/'.$img['id'].'.jpeg';
+            if(!is_file($filename)) {
+                savefile($img['orig'], $filename);
+            }
+            array_push($birds[$htmlfile]['img'], $img);
+        }
 
-		foreach(get_mp3(ROZHLAS.$clanek) as $mp3){
-			$filename=TMP.'/'.$mp3['id'].'.mp3';
-			if(!is_file($filename)){
-				savefile($mp3['url'], $filename);
-			}
-			array_push($birds[$htmlfile]['mp3'], $mp3);
-		}
+        foreach(get_mp3(ROZHLAS.$clanek) as $mp3) {
+            $filename = TMP.'/'.$mp3['id'].'.mp3';
+            if(!is_file($filename)) {
+                savefile($mp3['url'], $filename);
+            }
+            array_push($birds[$htmlfile]['mp3'], $mp3);
+        }
 
-		if(isset($rubriky[$birds[$htmlfile]['rubrikaid']])){
-			array_push($rubriky[$birds[$htmlfile]['rubrikaid']]['clenove'], $birds[$htmlfile]);
-		}else{
-			$rubriky[$birds[$htmlfile]['rubrikaid']] = array(
-				'jmeno' => $birds[$htmlfile]['rubrika'],
-				'clenove' => array($birds[$htmlfile]),
-			);
-		}
+        if(isset($rubriky[$birds[$htmlfile]['rubrikaid']])) {
+            array_push($rubriky[$birds[$htmlfile]['rubrikaid']]['clenove'], $birds[$htmlfile]);
+        } else {
+            $rubriky[$birds[$htmlfile]['rubrikaid']] = array(
+              'jmeno' => $birds[$htmlfile]['rubrika'],
+              'clenove' => array($birds[$htmlfile]),
+            );
+        }
 
-	}
+    }
 }
 
 uasort($birds, 'sort_by_jmeno');
@@ -66,67 +66,67 @@ uasort($rubriky, 'sort_by_jmeno');
 
 $cislo = 0;
 $seznamptaku = array();
-foreach($birds as $htmlfile => $bird){
-	$seznamptaku[$cislo] = array('file' => $htmlfile,
-		'bird' =>$bird);
-	$cislo++;
+foreach($birds as $htmlfile => $bird) {
+    $seznamptaku[$cislo] = array('file' => $htmlfile,
+      'bird' => $bird);
+    $cislo++;
 }
 
 $cislo = 0;
 
-foreach($birds as $htmlfile => $bird){
-	$smarty->assign('title', $bird['jmeno']);
-	$smarty->assign('title_ascii', asciize($bird['jmeno']));
+foreach($birds as $htmlfile => $bird) {
+    $smarty->assign('title', $bird['jmeno']);
+    $smarty->assign('title_ascii', asciize($bird['jmeno']));
 
-	$bird['clanek'] = array();
+    $bird['clanek'] = array();
 
-	foreach($bird['info'] as $info){
-		$bird['clanek'][$info['poradi']] = array('typ' => 'text', 'text' => $info['text']);
-	}
+    foreach($bird['info'] as $info) {
+        $bird['clanek'][$info['poradi']] = array('typ' => 'text', 'text' => $info['text']);
+    }
 
-	foreach($bird['img'] as $img){
-		$bird['clanek'][$img['poradi']] = array('typ' => 'img', 'img' => $img);
-		$smarty->assign('htmlfile', $htmlfile);
-		$smarty->assign('img', $img['id']);
-		$html = $smarty->fetch('img.tpl');
-		file_put_contents(WWW.'/'.$img['id'].'.html', $html);
-	}
+    foreach($bird['img'] as $img) {
+        $bird['clanek'][$img['poradi']] = array('typ' => 'img', 'img' => $img);
+        $smarty->assign('htmlfile', $htmlfile);
+        $smarty->assign('img', $img['id']);
+        $html = $smarty->fetch('img.tpl');
+        file_put_contents(WWW.'/'.$img['id'].'.html', $html);
+    }
 
-	foreach($bird['mp3'] as $mp3){
-		$bird['clanek'][$mp3['poradi']] = array('typ' => 'mp3', 'mp3' => $mp3);
-	}
+    foreach($bird['mp3'] as $mp3) {
+        $bird['clanek'][$mp3['poradi']] = array('typ' => 'mp3', 'mp3' => $mp3);
+    }
 
-	ksort($bird['clanek']);
+    ksort($bird['clanek']);
 
-	$smarty->assign('ptak', $bird);
-	if($cislo == 0){
-		$smarty->assign('prev', $seznamptaku[count($seznamptaku)-1]);
-	}else{
-		$smarty->assign('prev', $seznamptaku[$cislo-1]);
-	}
+    $smarty->assign('ptak', $bird);
+    if($cislo == 0) {
+        $smarty->assign('prev', $seznamptaku[count($seznamptaku) - 1]);
+    } else {
+        $smarty->assign('prev', $seznamptaku[$cislo - 1]);
+    }
 
-	if($cislo == count($seznamptaku)-1){
-		$smarty->assign('next', $seznamptaku[0]);
-	}else{
-		$smarty->assign('next', $seznamptaku[$cislo+1]);
-	}
-	$html = $smarty->fetch('hlavicka.tpl');
-	$html .= preg_replace('/\('.$bird['lat'].'\)/', '(<a href="lat.html#'.$bird['id'].'" class="lat">'.$bird['lat'].'</a>)', $smarty->fetch('ptak.tpl'));
-	$html .= $smarty->fetch('paticka.tpl');
-	file_put_contents(WWW.'/'.$htmlfile, $html);
-	$cislo++;
+    if($cislo == count($seznamptaku) - 1) {
+        $smarty->assign('next', $seznamptaku[0]);
+    } else {
+        $smarty->assign('next', $seznamptaku[$cislo + 1]);
+    }
+    $html = $smarty->fetch('hlavicka.tpl');
+    $html .= preg_replace('/\('.$bird['lat'].'\)/', '(<a href="lat.html#'.$bird['id'].'" class="lat">'.$bird['lat'].'</a>)', $smarty->fetch('ptak.tpl'));
+    $html .= $smarty->fetch('paticka.tpl');
+    file_put_contents(WWW.'/'.$htmlfile, $html);
+    $cislo++;
 }
 
-foreach($rubriky as $htmlfile => $rubrika){
+foreach($rubriky as $htmlfile => $rubrika) {
 
-	uasort($rubrika['clenove'], 'sort_by_jmeno');
+    uasort($rubrika['clenove'], 'sort_by_jmeno');
 
-	$smarty->assign('title', $rubrika['jmeno']);
-	$smarty->assign('rubrika', $rubrika);
-	$html = $smarty->fetch('hlavicka.tpl');
-	$html .= $smarty->fetch('rubrika.tpl');
-	$html .= $smarty->fetch('paticka.tpl');
-	file_put_contents(WWW."/$htmlfile.html", $html);
+    $smarty->assign('title', $rubrika['jmeno']);
+    $smarty->assign('rubrika', $rubrika);
+    $html = $smarty->fetch('hlavicka.tpl');
+    $html .= $smarty->fetch('rubrika.tpl');
+    $html .= $smarty->fetch('paticka.tpl');
+    file_put_contents(WWW."/$htmlfile.html", $html);
 }
 
 $smarty->assign('lat', $lat);
@@ -169,9 +169,9 @@ copyToDir('templates/*.js', WWW);
 copyToDir('templates/*.svg', WWW);
 copyToDir(TMP.'/*.jpeg', WWW);
 
-foreach($birds as $htmlfile => $bird){
-	foreach($bird['mp3'] as $mp3){
-		$foo = $mp3['id'].'.mp3';
-		copy(TMP."/$foo", WWW."/$foo");
-	}
+foreach($birds as $htmlfile => $bird) {
+    foreach($bird['mp3'] as $mp3) {
+        $foo = $mp3['id'].'.mp3';
+        copy(TMP."/$foo", WWW."/$foo");
+    }
 }
